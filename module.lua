4c73b5bd6ef35f48afd289bcebe9f20984975eeb
Segwardd <77902556+Segwardd@users.noreply.github.com>
2021-01-24 22:15:24 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,10 +1,13 @@
-print('module extention made by segward, Segward#7539')
+
 local module = {
     objects = function(self)
         metatable = setmetatable({}, {__index = self})
         self.table = {}
+        self.index = 1
+        self.index_object = nil
         self.add = function(arg1)
             table.insert(self.table,arg1)
+            self.index_object = self.table[self.index]
         end
         self.rem = function(arg1)
             for i,v in pairs(self.table) do
@@ -31,7 +34,42 @@ local module = {
             end
         end
         self.listen = function()
-            print(unpack(self.table))
+            non_table = {}
+            for i,v in pairs(self.table) do
+                if type(v) == 'table' then
+                    print('table',unpack(v))
+                else
+                    table.insert(non_table,v)
+                end 
+             end
+             print(unpack(non_table))
+        end
+        self.loop_condition = function(condition,aftermath)
+            function aftermath_()
+                if aftermath == 'Next' then
+                    self.index = self.index + 1
+                    self.index_object = self.table[self.index]
+                end
+            end
+            if condition[2] then 
+                if condition[2] == 'Equals' then
+                    if condition[1] == condition[3] then
+                        aftermath_()
+                    end
+                elseif condition[2] == 'Under' then
+                    if condition[1] <= condition[3] then
+                        aftermath_()
+                    end
+                elseif condition[2] == 'Above' then
+                    if condition[1] >= condition[3] then
+                        aftermath_()
+                    end
+                elseif condition[2] == 'Unequals' then
+                    if condition[1] ~= condition[3] then
+                        aftermath_()
+                    end
+                end
+            end
         end
         return metatable
     end
