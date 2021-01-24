6d984d4d60bcf0423b99b29cc31782c68f3eccba
Segwardd <77902556+Segwardd@users.noreply.github.com>
2021-01-24 14:16:20 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,4 +1,5 @@
-
+local setsimulation = setsimulationradius or set_simulation_radius
+setsimulation(1e308, 1/0)
 local module = {
     BodyPosition = function(self,Body,Position,Strength)
         object = setmetatable({}, {__index = self})
@@ -8,10 +9,6 @@ local module = {
         self.Strength = Strength
         self.Body.CFrame = self.Body.CFrame + (self.Position - self.Body.Position) * self.Strength*0.001
         return object
-    end
-    SetRadius = function()
-        local setsimulation = setsimulationradius or set_simulation_radius
-        setsimulation(1e308, 1/0)
     end
 }
 return module
