24bc29b8c886e8d39670c2a90bdc622787b8d50d
Segwardd <77902556+Segwardd@users.noreply.github.com>
2021-01-24 14:00:02 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,3 +1,18 @@
-local module = {1,2,3,4,5}
-
+local module = {
+    BodyPosition = function(self,Body,Position,Strength)
+        object = setmetatable({}, {__index = self})
+        self.Status = true
+        self.Body = Body
+        self.Position = Position
+        self.Strength = Strength
+        while true do
+        if self.Body.Parent:FindFirstChild('Humanoid') then
+            self.Body.Parent.Humanoid:ChangeState(11)
+        end
+        self.Body.CFrame = self.Body.CFrame + (self.Position - self.Body.Position) * self.Strength*0.001
+        wait()
+        end
+        return object
+    end
+}
 return module
