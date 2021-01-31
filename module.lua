92935d118dfb0eee08e7b20c9faa5a6b794302f6
Segwardd <77902556+Segwardd@users.noreply.github.com>
2021-01-31 15:57:37 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -35,17 +35,43 @@ setmetatable(module,{
         end
         object.ganp = function(Parent) -- ganp being game alive non players (returns all living instances within a table)
             local Return = {}
-            for i,Descendant in pairs(Parent) do
-                if Descendant:IsA('Humanoid') then
-                    local Parent = Descendant.Parent
-                    if game.Players:FindFirstChild(Parent.Name) then
+            for i,v in pairs(Parent) do
+                if v:FindFirstChild('Humanoid') then
+                    if game.Players:FindFirstChild(v.Name) then
                     else
-                        table.insert(Return,Parent)
+                        table.insert(Return,v)
                     end
                 end
             end
             return Return
         end
+        object.sortclass = function(table,class)
+            local Return = {}
+            for i,v in pairs(table) do
+                if type(v) == class then
+                        table.insert(Return,v)
+                end
+            end
+            return Return
+        end
+	object.sortname = function(table,name)
+		local Return = {}
+		for i,v in pairs(table) do
+			if v.Name == name then
+				table.insert(Return,v)
+			end
+		end
+		return Return
+	end
+	object.sortchild = function(table,child)
+		local Return = {}
+		for i,v in pairs(table) do
+			if v:FindFirstChild(child) then
+				table.insert(Return,v)
+			end
+		end
+		return Return
+	end
         object.tirc = function(condition) -- tirc standing for table index return condition (returns the index of the table, with a condition to pass index up)
             if condition then
                 local returning = object[object.index]
