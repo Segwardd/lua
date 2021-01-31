1522faa39394e62d34142066a6e864b4783ec8a3
Segwardd <77902556+Segwardd@users.noreply.github.com>
2021-01-31 04:30:04 +0100

    Update module.lua

--- module.lua
+++ module.lua
@@ -1,13 +1,21 @@
+  
 local module = {}
-
 setmetatable(module,{
 	__index = function(__table,index)
 		local object = setmetatable({},{__index = self,
 		    __add = function(self, value)
 		        table.insert(self, value)
 		        return self
-		    end
-		})
+			end,
+			__sub = function(self, value)
+				local table_find = table.find(self, value)
+				if table_find then
+					table.remove(self, table_find)
+					return self
+				end
+			end
+        })
+        object.index = 1
 		object.add = function(self,_table)
 			for i,v in ipairs(_table) do
 				self[#object+1] = v
@@ -20,6 +28,21 @@ setmetatable(module,{
 		      end
 		   end
 		end
+		object.tween_ = function(object_,vec,lookvec)
+            local Time = (object_.Position - vec).magnitude/vec.magnitude * 0.5
+			game:GetService('TweenService'):Create(object_,TweenInfo.new(Time, Enum.EasingStyle.Linear),{CFrame = CFrame.new(vec,lookvec)}):Play()
+		end
+        object.tirc = function(condition) -- tirc standing for table index return condition (returns the index of the table, with a condition to pass index up)
+            if condition then
+                local returning = object[object.index]
+                if object.index >= #object then
+                    return false
+                else
+                    object.index = object.index + 1
+                end
+            end
+            return object[object.index]
+        end
 		return object
 	end
 })
